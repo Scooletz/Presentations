ee4b52a4cf246d17306c16c6525d429987150f99
Scooletz <scooletz@gmail.com>
2016-03-05 21:53:14 +0100

    An initial set of presentations

---
+++ HttpCache/src/Tests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Cache.WebAPI;
+using Microsoft.Owin.Hosting;
+using NReco.PhantomJS;
+using NUnit.Framework;
+
+namespace Cache
+{
+    [TestFixture]
+    public class Tests : IDisposable
+    {
+        private readonly IDisposable _app;
+        private const string Address = "http://localhost:18458/";
+
+        public Tests()
+        {
+            _app = WebApp.Start<Startup>(Address);
+        }
+
+        [Test]
+        public void NoCache()
+        {
+            RunFor(DataController.RouteNoCache);
+        }
+
+        [Test]
+        public void Cache10s()
+        {
+            RunFor(DataController.RouteCache10s);
+        }
+
+        [Test]
+        public void ETag()
+        {
+            RunFor(DataController.RouteETag);
+        }
+
+        private static void RunFor(string routeNoCache)
+        {
+            var start = DataController.Calls;
+
+            var phantomJs = new PhantomJS();
+            phantomJs.OutputReceived += (sender, e) => Console.WriteLine("{0}", e.Data);
+            phantomJs.RunScript(GetJson(routeNoCache), null);
+
+            var end = DataController.Calls;
+
+            Console.WriteLine("Controller was called {0} times", end - start);
+        }
+
+        private static string GetJson(string address)
+        {
+            return @"var page = require('webpage').create(),
+                server = '" + Address + address + @"';
+                console.log('Opening following url twice: ' + server);
+                page.open(server, function (status) {
+                    if (status !== 'success') {
+                        console.log('get failed');
+                    } else {
+                        console.log(page.content);
+                    }
+
+                    page.open(server, function (status) {
+                        if (status !== 'success') {
+                            console.log('get failed');
+                        } else {
+                            console.log(page.content);
+                        }
+                        phantom.exit();
+                        });
+                });";
+        }
+
+        public void Dispose()
+        {
+            _app.Dispose();
+        }
+    }
+}
---
+++ HttpCache/src/WebAPI/DataController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Cache.WebAPI
+{
+    public class DataController : ApiController
+    {
+        public const string RouteNoCache = "api/nocache";
+        public const string RouteCache10s = "api/cache10s";
+        public const string RouteETag = "api/etag";
+
+        public static int Calls = 0;
+
+        [Route(RouteNoCache), HttpGet]
+        public HttpResponseMessage NoCache()
+        {
+            var msg = GetResponse();
+            msg.Headers.CacheControl = new CacheControlHeaderValue
+            {
+                NoCache = true
+            };
+            return msg;
+        }
+
+        [Route(RouteCache10s), HttpGet]
+        public HttpResponseMessage CacheFor10Seconds()
+        {
+            var msg = GetResponse();
+            msg.Headers.CacheControl = new CacheControlHeaderValue
+            {
+                MaxAge = TimeSpan.FromSeconds(10),
+                Public = true
+            };
+            return msg;
+        }
+
+        private const string Tag = @"""ETagValue1""";
+
+        [Route(RouteETag), HttpGet]
+        public HttpResponseMessage ETag()
+        {
+            // instead of this silly check against const value, one could issue a following SQL for instance:
+            // SELECT * FROM X WHERE id=@id AND version <> @etag
+            //
+            // that would get data only if the version is different from the tag
+
+            var hasValidTag = Request.Headers.IfNoneMatch.Any(value => value.Tag == Tag);
+            HttpResponseMessage msg;
+            
+            if (hasValidTag)
+            {
+                Calls += 1;
+                msg = Request.CreateResponse(HttpStatusCode.NotModified);
+            }
+            else
+            {
+                msg = GetResponse();
+            }
+            
+            msg.Headers.ETag = new EntityTagHeaderValue(Tag);
+            return msg;
+        }
+
+        private HttpResponseMessage GetResponse()
+        {
+            Calls += 1;
+            var msg = Request.CreateResponse(new { Value = Calls });
+            return msg;
+        }
+    }
+}
